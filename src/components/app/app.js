a06e16a0ccc70eb9c347db6c2370e47315b66b45
bahuzz <dbahuzz@gmail.com>
2019-11-30 10:05:06 +0300

    switch added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
-import {Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import Background from './food-bg.jpg';
 import RestoServiceContext from '../resto-service-context';
 
@@ -9,8 +9,10 @@ const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <Route path="/" exact component={MainPage}/>
-            <Route path="/cart" component={CartPage}/>
+            <Switch>
+                <Route path="/" exact component={MainPage}/>
+                <Route path="/cart" component={CartPage}/>
+            </Switch>
         </div>
     )
 }
